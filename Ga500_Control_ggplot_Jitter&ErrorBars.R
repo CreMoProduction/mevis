e597b487d01eb33bd0d08fc81adf3f248fcccb42
CreMoProduction <aleks.delta2013@gmail.com>
2021-10-20 18:41:31 +0200

    v.0.1

--- Ga500_Control_ggplot_Jitter&ErrorBars.R
+++ Ga500_Control_ggplot_Jitter&ErrorBars.R
@@ -1,14 +1,26 @@
-#for (x in dice) {
-#  print(x)
-#}
+#mevis v0.1 alpha
 
 library("readxl")
-dataset <- read_excel("D:\\Ga Processed Data.xlsx", sheet ="SIMCA 2.1")
-
 library(gridExtra)
 library(ggplot2)
 library(dplyr)
-Metabolite<-19
+#----------------Пользовательские переменные 
+Difference=2
+Pvalue=0.5
+Metabolite<-8
+dataset <- read_excel("D:\\Ga Processed Data.xlsx", sheet ="SIMCA 2.1")
+#----------------Объявляю глобальные переменные
+Ncol= ncol(dataset)
+metabolitedata <- NULL
+predataMetaboliteName<- c()
+predata <- c()
+data <- c()
+nrowGaN=NULL        #кол-во строк Галлий
+nrowCtrlN=NULL      #кол-во строк контроля
+nrowTotalN=NULL     #кол-во строок суммарно
+meanGaN
+meanCtrlN
+#---------------- 
 
 #------ базовая фигня для данных
   colnames(dataset)
@@ -18,16 +30,23 @@ Metabolite<-19
   TotalMetabolites
 
 
-#Основная ф-ция постройки графика
-Plot_Fun <- function(Metabolite) {
+
+
+#Ф-ция подготовки данных
   
-  print(paste("plot Metabolite No", Metabolite))
+#---- получаю названия групп Control, Ga500
+gaN<- data.frame(dataset[31:36, 4])
+ctrlN<- data.frame(dataset[1:6, 4])
+chemicalN_column<- rbind(gaN, ctrlN)
+chemicalN_column
+nrowGaN= nrow(gaN)
+nrowCtrlN= nrow(ctrlN)
+nrowTotalN=nrow(chemicalN_column)
+
+
+Data_Fun <- function(Metabolite) {
   
-  #---- получаю названия групп Control, Ga500
-  gaN<- data.frame(dataset[31:36, 4])
-  ctrlN<- data.frame(dataset[1:6, 4])
-  chemicalN_column<- rbind(gaN, ctrlN)
-  chemicalN_column
+  #print(paste("plot Metabolite No", Metabolite))
   
   #---- получаю величину  пика
   gaValue<- data.frame(dataset[31:36, Metabolite])
@@ -37,38 +56,87 @@ Plot_Fun <- function(Metabolite) {
   chemicalValue_column <-rbind(gaValue, ctrlValue)
   chemicalValue_column
   
-  #---- получаю имя метаболита
+  #---- получаю имя метаболита МОЖНО УДАЛИТЬ
   columnName<-dimnames(chemicalValue_column)
   metaboliteName<-toString(columnName[2])
   metaboliteName
   #---- сливаю 2 столбца в таблицу
-  plotdata<- data.frame(chemicalN_column, chemicalValue_column)
-  plotdata
-  colnames(plotdata)[2]="value"
-  colnames(plotdata)[2]
-  #plotdata<-mutate(Q=chemicalN_column, W=chemicalValue_column)
+  metabolitedata<- data.frame(chemicalN_column, chemicalValue_column)
+  metabolitedata
+  colnames(metabolitedata)[2]="value"
+  colnames(metabolitedata)[2]
+  #metabolitedata<-mutate(Q=chemicalN_column, W=chemicalValue_column)
   #---
+  return(metabolitedata)
+}    
+#------
+rm(predata)
+rm(predataMetaboliteName)
+predata
+predataMetaboliteName
+for (i in 8:Ncol) {
+  Metabolite=i
+  metabolitedata=Data_Fun(i)
+  predata <-c(predata, metabolitedata)
+  #print(predata)
+  metaboliteName<-colnames(dataset)[i]
+  #print(metaboliteName)
+  predataMetaboliteName=c(predataMetaboliteName, metaboliteName)
+}
+
+#считаю среднее для метаболита и ищу различия
+rm(data)
+data <- c()
+data= c(data, predata[1])
+for (i in 1:length(predata)) {
+  if((i %% 2) == 0) {
+    df= data.frame(predata[i])
+    meanGaN=mean(df[1:nrowGaN,])
+    meanCtrlN=mean(df[nrowGaN+1:nrowGaN,])
+    DifferenceNup=meanGaN/meanCtrlN
+    DifferenceNdown=meanCtrlN/meanGaN
+    tTest=t.test(cbind(data.frame(df[1:nrowGaN,]), data.frame(df[nrowGaN+1:nrowGaN,])))[["p.value"]]
+    DifferenceNup
+    DifferenceNdown
+    
+    if (DifferenceNup>=Difference | DifferenceNdown>=Difference & tTest<=Pvalue){ 
+           data= c(data, predata[i])
+           print(paste("No",i,"| p value ",tTest, ". Difference Up and Down", DifferenceNup, " ", DifferenceNdown))
+           } else {0}
+  } else {
+    print(paste("No",i,"| does not meet criterias"))
+  }
   
-  
-  
-  g <- ggplot(plotdata, aes(x=Chemical, y=value, color=Chemical, shape=Chemical))
-    g + theme_classic()+
-    geom_boxplot(color = "grey70", width=0.2)+
-    geom_jitter(width = 0.25, size=3)+ 
-    labs(subtitle=metaboliteName, 
-        y="y", 
-        x="",
-       title="Control and GaCit 500 uM of the"
-       )+
-    theme(plot.title = element_text(size=10), 
+}
+
+for (i in 1:length(data)) {}
+plotdata=cbind(chemicalN_column, data.frame(data[2]))
+
+g <- ggplot(plotdata, aes(x=Chemical, y=value, color=Chemical, shape=Chemical))
+g + theme_classic()+
+  geom_boxplot(color = "grey70", width=0.2)+
+  geom_jitter(width = 0.25, size=3)+ 
+  labs(subtitle="metaboliteName", 
+       y="y", 
+       x="",
+       title=paste("p value" )
+  )+
+  theme(plot.title = element_text(size=10), 
         plot.subtitle = element_text(size=10),
         legend.position = "none") #удаляю легенду
-    #geom_text(aes(label=round(value, 2)), size=3)+ #указываю величину площади пика и округляю ее
-    #ylim(0, 5000) #указываю мин макс значения для y axis
-    
-}    
+#geom_text(aes(label=round(value, 2)), size=3)+ #указываю величину площади пика и округляю ее
+#ylim(0, 5000) #указываю мин макс значения для y axis
 
-#------
+
+
+
+
+
+
+{#ggplot
+
+
+
 p <- list()
 for (i in 1:10){
   Metabolite=i
@@ -78,9 +146,9 @@ for (i in 1:10){
 }
 do.call(grid.arrange, c(p, ncol = 4))
 #------
+}
 
-gaValue
-ctrlValue
+#подготавливаю данные для t-теста
 
 gaValue_t_test= gaValue
 colnames(gaValue_t_test)<- "gaValue"
@@ -92,18 +160,20 @@ ctrlValue_t_test
 
 df_t_test= cbind(gaValue_t_test, ctrlValue_t_test)
 df_t_test
-t(df_t_test)
+#t(df_t_test)  #транспонировать
 colnames(df_t_test)
 
 
-t.test(gaValue)
+t.test(df_t_test)
+t.test(df_t_test)[["p.value"]]
+ifelse(t.test(df_t_test)[["p.value"]]< 0.05,
+       1,
+       0
+       )
 
 
 
-set.seed(1)
-x <- sample(1:10, 200, TRUE)
-y <- 3 * x + rnorm(200, 0, 5)
-plot(y ~ jitter(x, 1), pch = 15)
+
 
 rm(Q)
 
